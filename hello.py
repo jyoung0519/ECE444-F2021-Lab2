588233caba0ec8e3a9acf7f09e12472567e069e7
Joseph Yang <josephyang519@gmail.com>
2021-09-24 14:42:42 -0400

    Added error handling for pages, added forms with CSRF protection

--- hello.py
+++ hello.py
@@ -1,16 +1,42 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-app = Flask(__name__)
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+    
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time = datetime.utcnow())
+
+@app.errorhandler(404)
+def page_not_found(e):
+ return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+ return render_template('500.html'), 500
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators = [Required()])
+    submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     app.run(debug=True)
