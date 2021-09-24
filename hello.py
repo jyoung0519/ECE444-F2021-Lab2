4001df648dadaebd7fe3622dbbf7fd95a5b3a4fb
Joseph Yang <josephyang519@gmail.com>
2021-09-24 14:53:02 -0400

    Added message flashing on updates

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask import session, redirect, url_for, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -15,12 +16,15 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+        form = form, name = session.get('name'))
     
 
 @app.route('/user/<name>')
